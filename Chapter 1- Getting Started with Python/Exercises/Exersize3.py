96e129a562b9fc2799e96d68e0fc35298fd4d537
mark <144319737+Noramite@users.noreply.github.com>
2023-09-30 15:42:13 +0400

    Update Exersize3.py

--- Chapter 1- Getting Started with Python/Exercises/Exersize3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exersize3.py
@@ -5,7 +5,7 @@ time = datetime.now()
 
 # Cosmetic changes that make the Date appear in text form instead of just numbers
 DMY = current.strftime ("%d %B, %Y")
-time = time.strftime ("%H:%M.%S")
+time = time.strftime ("%I:%M %p")
 
 print ("The Date Today is", DMY)
 print ("The time right now is", time)
