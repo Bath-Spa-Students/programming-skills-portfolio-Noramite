31ad8fc3205e368b55954daa66c4bf3f6022ffae
mark <144319737+Noramite@users.noreply.github.com>
2023-09-30 17:52:12 +0400

    Exercise5

---
+++ Chapter 1- Getting Started with Python/Exercises/Exersize5.py
@@ -0,0 +1,8 @@
+import math
+
+Number = float(input("Provide the radius of the circle "))
+
+squared = (Number*Number)
+Area = (squared*math.pi)
+
+print ("the area of the Circle with the radius of", Number, "is", Area)
