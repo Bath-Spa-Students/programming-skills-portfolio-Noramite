04db6e70c00d3adf706543fe6b03d1440ed16883
mark <144319737+Noramite@users.noreply.github.com>
2023-09-30 17:32:22 +0400

    Exercise5

---
+++ Chapter 1- Getting Started with Python/Exercises/Exersize4.py
@@ -0,0 +1,5 @@
+Colour = (input("What is your favorite colour? "))
+Show = (input("What is your favourite show? "))
+Game = (input("What is your favourite game? "))
+
+print ("I see that you like the colour " + Colour,", and that you like the show " + Show,". I also think that " + Game,"is a good game.")
