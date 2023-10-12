5f75dfed21ff914bff711872be92e5f75d06ad6a
mark <144319737+Noramite@users.noreply.github.com>
2023-10-12 11:28:24 +0400

    Exercise 3

--- Chapter 2- Variables & Comments/Exercises/Exercise3.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -2,15 +2,13 @@ name1 = "Jane"
 name2 = "Doe"
 
 print (name1+'\n'+' '+name2)
-
 print (name1+'\t'+' '+name2)
 
+#========================second part of the exercise=============================================================
 rofl = "                                           The world is mine                              "
 
 lstrip = rofl.lstrip()
-
 rstrip =rofl.rstrip()
-
 strip =rofl.strip()
 
 print (lstrip)
