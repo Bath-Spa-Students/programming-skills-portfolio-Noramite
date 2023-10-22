182d3ee986fb77886ca2396edba083bd48b220cf
mark <144319737+Noramite@users.noreply.github.com>
2023-10-22 22:39:00 +0400

    Exercise 2

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,24 @@
+names = ["Ethan Williams", "Olivia Anderson", "Jackson Smith", "Sophia Taylor", "Noah Johnson", "Emma Brown", "Liam Davis", "Ava Wilson", "Mason Martinez", "Isabella Jones", "Caleb Anderson", "Mia Thompson", "Aiden White", "Harper Harris", "Samuel Robinson"]
+
+#this is for accessing one by one
+print (f"Hi,{names[0]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[1]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[2]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[3]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[4]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[5]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[6]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[7]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[8]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[9]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[10]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[11]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[12]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[13]}. its nice to finally meet you this lovely day")
+print (f"Hi,{names[14]}. its nice to finally meet you this lovely day")
+
+def message ():
+    print (f"Hi,{names}. its nice to finally meet you this lovely day")
+
+for names in names:
+    message()
