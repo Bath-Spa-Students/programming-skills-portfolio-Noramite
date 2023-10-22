bcecd31d012556d2267210c63688735b98099520
mark <144319737+Noramite@users.noreply.github.com>
2023-10-22 22:39:09 +0400

    Exercise 3

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+sussytranspo = ["Toyota Camry", "Ford Mustang", "Honda Civic", "Harley-Davidson Sportster", "Kawasaki Ninja", "Ducati Panigale"]
+
+intro = ("I would like to own a")
+
+for sussytranspo in sussytranspo:
+    print(intro, sussytranspo)
